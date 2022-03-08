dce4483fa44a3634b29ffeba76bc667e928c240f
TaraMKelly <tara.kelly16@gmail.com>
2022-03-08 16:03:47 -0500

    built components and initial fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import DishContainer from "./components/DishContainer";
+import NewDishForm from "./components/NewDishForm";
+import { useState, useEffect } from "react"
 
 function App() {
+  const [dishes, setDishes] = useState([])
+
+  useEffect(() => {
+    fetch('/dishes')
+      .then(res => res.json())
+      .then((data) => setDishes(data))
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewDishForm dishes={dishes} setDishes={setDishes}/>
+      <DishContainer dishes={dishes} setDishes={setDishes}/>
     </div>
   );
 }
---
+++ client/src/components/NewDishForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NewDishForm() {
+  return (
+    <div>NewDishForm</div>
+  )
+}
+
+export default NewDishForm
